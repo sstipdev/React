4babe65b9cf300ca8d1a86d4bd82bb9c7f73a732
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-17 22:25:42 +0900

    State 문법 기초

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   const post = "강남 테헤란로";
+  const [coat, a] = useState("가을 코트 추천");
+  const [food, b] = useState("강남 우동맛집");
+  const [python, c] = useState("파이썬 독학");
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: "green", fontSize: "18px;" }}>블로그</h4>
+        <h4 style={{ color: "green", fontSize: "18px;" }}>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>{coat}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{food}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{python}</h4>
+        <p>2월 17일 발행</p>
       </div>
       <h4>{post}</h4>
     </div>
