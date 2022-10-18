3776cd88c2a01b496960f06c48eec478410c43e6
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-18 21:30:09 +0900

    컴포넌트 문법

--- src/App.js
+++ src/App.js
@@ -51,7 +51,6 @@ function App() {
         </h4>
         <p>2월 17일 발행</p>
       </div>
-
       <div className="list">
         <h4>{arr[1]}</h4>
         <p>2월 17일 발행</p>
@@ -61,8 +60,19 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <h4>{post}</h4>
+      <Modal></Modal>
     </div>
   );
 }
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>내용</p>
+    </div>
+  );
+};
 
 export default App;
